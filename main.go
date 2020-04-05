cd12b85ef2271371a9eafeb628a5ab8fe7eb5e8e
Cedric Chee <cedric@invictusbyte.com>
2020-04-05 20:03:52 +0800

    Customize HTTP headers
    * HTTP status codes
    * Customize headers
    * The http.Error shortcut

--- main.go
+++ main.go
@@ -27,6 +27,22 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 
 // Add a createSnippet handler function.
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	// If it's not, use the w.WriteHeader() method to send a 405 status code and
+	// the w.Write() method to write a 'Method Not Allowed' response body. We
+	// then return from the function so that the subsequent code is not executed.
+	if r.Method != "POST" {
+		// Use the Header().Set() method to add an 'Allow: POST' header to the
+		// response header map. The first parameter is the header name, and
+		// the second parameter is the header value.
+		w.Header().Set("Allow", "POST")
+
+		// Use the http.Error() function to send a 405 status code and "Method Not
+		// Allowed" string as the response body.
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
