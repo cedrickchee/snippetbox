2b0f12ddfb886942d5799e2b492669c0a339a2e2
Cedric Chee <cedric@invictusbyte.com>
2020-04-06 23:49:18 +0800

    Catch runtime error

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -39,9 +40,21 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 		return
 	}
 
+	// Initialize a new buffer.
+	buf := new(bytes.Buffer)
+
 	// Execute the template set, passing in any dynamic data.
-	err := ts.Execute(w, td)
+	// Write the template to the buffer, instead of straight to the
+	// http.ResponseWriter. If there's an error, call our serverError helper and then
+	// return.
+	err := ts.Execute(buf, td)
 	if err != nil {
 		app.serverError(w, err)
+		return
 	}
+
+	// Write the contents of the buffer to the http.ResponseWriter. Again, this
+	// is another time where we pass our http.ResponseWriter to a function that
+	// takes an io.Writer.
+	buf.WriteTo(w)
 }
