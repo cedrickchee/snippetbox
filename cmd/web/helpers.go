1462db544fd9c3ee42d2005970a004b4df43905f
Cedric Chee <cedric@invictusbyte.com>
2020-04-06 23:58:12 +0800

    Common dynamic data

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 // The serverError helper writes an error message and stack trace to the errorLog,
@@ -43,11 +44,12 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 	// Initialize a new buffer.
 	buf := new(bytes.Buffer)
 
-	// Execute the template set, passing in any dynamic data.
+	// Execute the template set, passing the dynamic data with the current
+	// year injected.
 	// Write the template to the buffer, instead of straight to the
 	// http.ResponseWriter. If there's an error, call our serverError helper and then
 	// return.
-	err := ts.Execute(buf, td)
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 		return
@@ -57,4 +59,16 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 	// is another time where we pass our http.ResponseWriter to a function that
 	// takes an io.Writer.
 	buf.WriteTo(w)
+}
+
+// Create an addDefaultData helper. This takes a pointer to a templateData
+// struct, adds the current year to the CurrentYear field, and then returns
+// the pointer. Again, we're not using the *http.Request parameter at the
+// moment, but we will do later in the book.
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
 }
