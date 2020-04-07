0db86b8bfd6514da8da900d24caa4d8e855b4d68
Cedric Chee <cedric@invictusbyte.com>
2020-04-07 14:12:29 +0800

    Composable middleware chains

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
 	// Use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
@@ -20,9 +28,6 @@ func (app *application) routes() http.Handler {
 	// '/static' prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	// Pass the servemux as the 'next' parameter to the secureHeaders middleware.
-	// Because secureHeaders is just a function, and the function returns a
-	// http.Handler we don't need to do anything else. Wrap the existing chain
-	// with the logRequest middleware.
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	// Return the 'standard' middleware chain followed by the servemux.
+	return standardMiddleware.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/justinas/alice v1.2.0
 	google.golang.org/api v0.21.0 // indirect
 )
