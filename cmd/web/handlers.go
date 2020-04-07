d5c6752823727f97aa68ce1bb865382d0baf1bbb
Cedric Chee <cedric@invictusbyte.com>
2020-04-07 21:27:08 +0800

    Set up a form
    * Processing forms

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -63,7 +63,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create a new snippet..."))
+	app.render(w, r, "create.page.tmpl", nil)
 }
 
 // createSnippet is a handler function.
