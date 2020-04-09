6756039bc3417d009844f95a5ac0c144b7bf801f
Cedric Chee <cedric@invictusbyte.com>
2020-04-09 14:16:23 +0800

    User login
    Verifying the user details.

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -48,7 +48,29 @@ func (m *UserModel) Insert(name, email, password string) error {
 // Authenticate method verifies whether a user exists with the provided email
 // address and password. This will return the relevant user ID if they do.
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	// Retrieve the id and hashed password associated with the given email. If no
+	// matching email exists, we return the ErrInvalidCredentials error.
+	var id int
+	var hashedPassword []byte
+	row := m.DB.QueryRow("SELECT id, hashed_password FROM users WHERE email = ?", email)
+	err := row.Scan(&id, &hashedPassword)
+	if err == sql.ErrNoRows {
+		return 0, models.ErrInvalidCredentials
+	} else if err != nil {
+		return 0, err
+	}
+
+	// Check whether the hashed password and plain-text password provided match.
+	// If they don't, we return the ErrInvalidCredentials error.
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err == bcrypt.ErrMismatchedHashAndPassword {
+		return 0, models.ErrInvalidCredentials
+	} else if err != nil {
+		return 0, err
+	}
+
+	// Otherwise, the password is correct. Return the user ID.
+	return id, nil
 }
 
 // Get method fetches details for a specific user based on their user ID.
